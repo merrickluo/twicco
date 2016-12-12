2f1ba8e0557e378e647f6d1d4b87f180a2719211
A.I <merrick@luois.me>
2016-12-13 00:57:47 +0800

    better compose ui and take picture

--- app/src/main/kotlin/ninja/luois/twicco/compose/provider/NewTweetProvider.kt
+++ app/src/main/kotlin/ninja/luois/twicco/compose/provider/NewTweetProvider.kt
@@ -1,23 +1,43 @@
 package ninja.luois.twicco.compose.provider
 
+import android.app.Application
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.squareup.picasso.Picasso
+import com.twitter.sdk.android.core.TwitterApiClient
 import com.twitter.sdk.android.core.TwitterCore
-import com.twitter.sdk.android.core.services.StatusesService
 import ninja.luois.twicco.extension.observable.bgSingle
+import okhttp3.MediaType
+import okhttp3.RequestBody
 import rx.Single
+import java.io.ByteArrayOutputStream
+import java.io.File
+
+
+class ComposingTweet {
+    var text: String? = null
+    var mediaIds: List<String> = emptyList()
+}
 
 object NewTweetProvider {
 
-    val service: StatusesService
+    val client: TwitterApiClient
 
     init {
-        service = TwitterCore.getInstance().apiClient.statusesService
+        client = TwitterCore.getInstance().apiClient
     }
 
-    fun tweet_(text: String): Single<Unit> {
+
+    fun tweet_(tweet: ComposingTweet): Single<Unit> {
         return bgSingle { s ->
             try {
-                val resp = service.update(text, null, null, null, null, null, null, null, null)
+                val mediaIds = tweet.mediaIds.joinToString(",")
+
+                val resp = client.statusesService.update(tweet.text,
+                        null, null, null, null, null, null, null, mediaIds)
                         .execute()
+
                 if (resp.isSuccessful) {
                     s.onSuccess(Unit)
                 } else {
@@ -28,4 +48,43 @@ object NewTweetProvider {
             }
         }
     }
+
+    fun upload_(filepath: String): Single<String> {
+        return bgSingle { s ->
+            try {
+                val image = resizeImage(filepath)
+                Log.e("Tweet", "image size ${image.size}")
+                val body = RequestBody.create(MediaType.parse("image/webp"), image)
+                val resp = client.mediaService.upload(body, null, null)
+                        .execute()
+
+                if (resp.isSuccessful) {
+                    s.onSuccess(resp.body().mediaIdString)
+                } else {
+                    s.onError(Exception(resp.errorBody().string()))
+                }
+            } catch (tr: Throwable) {
+                s.onError(tr)
+            }
+        }
+    }
+
+    fun resizeImage(filepath: String): ByteArray {
+        val b = BitmapFactory.decodeFile(filepath)
+        val out = ByteArrayOutputStream()
+        b.compress(Bitmap.CompressFormat.WEBP, 50, out)
+        return out.toByteArray()
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/kotlin/ninja/luois/twicco/compose/view/NewTweetActivity.kt
+++ app/src/main/kotlin/ninja/luois/twicco/compose/view/NewTweetActivity.kt
@@ -1,18 +1,37 @@
 package ninja.luois.twicco.compose.view
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.jakewharton.rxbinding.view.clicks
+import com.jakewharton.rxbinding.view.enabled
+import com.jakewharton.rxbinding.widget.text
+import com.jakewharton.rxbinding.widget.textChanges
+import com.squareup.picasso.Picasso
 import com.trello.rxlifecycle.kotlin.bindToLifecycle
 import kotterknife.bindView
 import ninja.luois.twicco.R
 import ninja.luois.twicco.common.Activity
+import ninja.luois.twicco.compose.provider.ComposingTweet
 import ninja.luois.twicco.compose.provider.NewTweetProvider
 import ninja.luois.twicco.extension.observable.subscribeTo
 import ninja.luois.twicco.extension.observable.withDialog
 import rx.android.schedulers.AndroidSchedulers
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 private fun String.isValidTweet(): Boolean {
     return isNotEmpty() && length < 140;
@@ -20,8 +39,19 @@ private fun String.isValidTweet(): Boolean {
 
 class NewTweetActivity : Activity() {
 
+    //Constants
+    val kRequestPermissionCamera = 101
+    val kRequestPermissionPhoto = 102
+
+    val kRequestCamera = 201
+    val kRequestPhoto = 202
+
     val tweetEditText by bindView<EditText>(R.id.text_tweet)
     val tweetButton by bindView<Button>(R.id.button_tweet)
+    val tweetCountText by bindView<TextView>(R.id.text_tweet_count)
+    val imageButton by bindView<Button>(R.id.button_image)
+
+    val tweet = ComposingTweet()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,25 +61,173 @@ class NewTweetActivity : Activity() {
         tweetButton.clicks()
                 .bindToLifecycle(this)
                 .subscribe {
-                    tweet(tweetEditText.text.toString())
+                    tweet()
+                }
+
+        tweetEditText.textChanges()
+                .bindToLifecycle(this)
+                // save to composing tweet
+                .doOnNext { tweet.text = it.toString() }
+                .map { it.toString().isValidTweet() }
+                .subscribe(tweetButton.enabled())
+
+        tweetEditText.textChanges()
+                .bindToLifecycle(this)
+                .map { (140 - it.length).toString() }
+                .subscribe(tweetCountText.text())
+
+        imageButton.clicks()
+                .bindToLifecycle(this)
+                .subscribe {
+                    showImageDialog()
                 }
     }
 
-    private fun tweet(text: String) {
-        //TODO: validate on the fly
-        if (text.isValidTweet()) {
-            NewTweetProvider.tweet_(text)
-                    .withDialog(this, "Sending")
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeTo {
-                        success = {
-                            Toast.makeText(this@NewTweetActivity, "success", Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                        error = {
-                            Toast.makeText(this@NewTweetActivity, "failed", Toast.LENGTH_SHORT).show()
-                        }
+    private fun showImageDialog() {
+        AlertDialog.Builder(this)
+                .setTitle("add an image")
+                .setItems(arrayOf("take photo", "pick from gallery")) { dialog, index ->
+                    when (index) {
+                        0 -> startTakingPhoto()
+                        1 -> startPickingImage()
                     }
+                }
+                .show()
+    }
+
+    private fun startPickingImage() {
+        if (!ensurePermission(Manifest.permission.CAMERA, kRequestPermissionCamera)) return
+    }
+
+    private fun startTakingPhoto() {
+        if (!ensurePermission(Manifest.permission.CAMERA, kRequestPermissionCamera)) return
+
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            val photoFile = createImageFile()
+            val uri = FileProvider.getUriForFile(this,"ninja.luois.twicco.fileprovider", photoFile)
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+            startActivityForResult(takePictureIntent, kRequestCamera)
         }
     }
+
+    private fun upload(filepath: String) {
+        NewTweetProvider.upload_(filepath)
+                .bindToLifecycle(this)
+                .withDialog(this, "uploading")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeTo {
+                    success = {
+                        tweet.mediaIds += it
+                        imageButton.text = tweet.mediaIds.size.toString()
+                    }
+                    error = {
+                        Toast.makeText(this@NewTweetActivity, "cannot upload image", Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+    fun ensurePermission(permission: String, requestCode: Int): Boolean {
+        if (ContextCompat.checkSelfPermission(this, permission)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
+            return false
+        }
+        return true
+    }
+
+    private fun tweet() {
+        NewTweetProvider.tweet_(tweet)
+                .bindToLifecycle(this)
+                .withDialog(this, "Sending")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeTo {
+                    success = {
+                        Toast.makeText(this@NewTweetActivity, "success", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                    error = {
+                        Toast.makeText(this@NewTweetActivity, "failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+
+    var currentTakingPhoto: String? = null
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentTakingPhoto = image.absolutePath
+        return image
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != RESULT_OK) return
+
+        when (requestCode) {
+            kRequestCamera -> {
+                currentTakingPhoto?.let {
+                    val f = File(it)
+                    if (f.exists() && f.length() > 3*1024*1024) {
+                    }
+                    upload(it)
+                }
+            }
+            kRequestPhoto -> {
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            kRequestPermissionCamera -> {
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startTakingPhoto()
+                } else {
+                    Toast.makeText(this@NewTweetActivity,
+                            "cannot take photo without permission.",Toast.LENGTH_SHORT)
+                            .show()
+                }
+            }
+            kRequestPermissionPhoto -> {
+                if (grantResults.isNotEmpty() &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startPickingImage()
+                } else {
+                    Toast.makeText(this@NewTweetActivity,
+                            "cannot pick image without permission.",Toast.LENGTH_SHORT)
+                            .show()
+                }
+            }
+        }
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
