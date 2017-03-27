f29f8a07918fb3e374cd3a927d8ea5c282abdde5
A.I <merrick@luois.me>
2017-03-27 18:03:46 +0800

    make sure use space in js project

---
+++ .dir-locals.el
@@ -0,0 +1,25 @@
+;;; .dir-locals.el --- summary
+;; Description:
+;;
+;; Author: A.I.
+;; Email: merrick@luois.me
+;; Last modified: <2017-03-27 18:03:30 Monday by merrick>
+;; Copyright (C) 2017 A.I. all rights reserved.
+;; PUBLIC LICENSE: GPLv3
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change log:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+((nil . ((indent-tabs-mode . nil)
+				 (fill-column . 80))))
+
+(provide '.dir-locals)
+;;; .dir-locals.el ends here
