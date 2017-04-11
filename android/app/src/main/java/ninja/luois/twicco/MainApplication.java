3c87fafa5a210f1caa61653e857f929a5dd96335
A.I <merrick@luois.me>
2017-04-12 00:18:00 +0800

    add react-native-twitter

--- android/app/src/main/java/ninja/luois/twicco/MainApplication.java
+++ android/app/src/main/java/ninja/luois/twicco/MainApplication.java
@@ -3,6 +3,7 @@ package ninja.luois.twicco;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import ga.piroro.rnt.RNTPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           FabricTwitterKitPackage.getInstance(),
           new MainReactPackage(),
+            new RNTPackage(),
             new VectorIconsPackage(),
           new LinearGradientPackage()
       );
