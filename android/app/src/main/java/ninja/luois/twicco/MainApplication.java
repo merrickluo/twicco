f9960e67e75bdacdf86b9984ea4387618fb6e0d4
A.I <merrick@luois.me>
2017-04-12 22:12:03 +0800

    add react-native-config

--- android/app/src/main/java/ninja/luois/twicco/MainApplication.java
+++ android/app/src/main/java/ninja/luois/twicco/MainApplication.java
@@ -3,6 +3,7 @@ package ninja.luois.twicco;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import ga.piroro.rnt.RNTPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeConfigPackage(),
           new RNTPackage(),
           new VectorIconsPackage(),
           new LinearGradientPackage()
